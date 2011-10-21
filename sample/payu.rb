e0d02fc8a3a1dca9639e5864f3b3fafe650069ac
michaltaberski <xtrol87@gmail.com>
2011-10-21 12:01:58 +0200

    test_0

--- sample/payu.rb
+++ sample/payu.rb
@@ -2,6 +2,8 @@ require "rubygems"
 require "./config"
 
 require "active_merchant"
+require "active_merchant/billing/payu"
+
 
 ActiveMerchant::Billing::Base.mode = :test
 gateway = ActiveMerchant::Billing::PayuGateway.new()
