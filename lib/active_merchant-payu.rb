c3e82697eab604273cd3cbf535b0997622032a42
michaltaberski <xtrol87@gmail.com>
2011-10-21 10:16:47 +0200

    initcommit

---
+++ lib/active_merchant-payu.rb
@@ -0,0 +1,7 @@
+require "active_merchant-payu/version"
+
+module Activemerchant
+  module Payu
+    # Your code goes here...
+  end
+end
---
+++ lib/active_merchant-payu/version.rb
@@ -0,0 +1,5 @@
+module Activemerchant
+  module Payu
+    VERSION = "0.0.1"
+  end
+end
